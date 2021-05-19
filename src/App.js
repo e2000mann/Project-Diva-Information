e271b52ad7496ee6f48cd06b63cf1da06597dfb2
up887818 <57417766+up887818@users.noreply.github.com>
2021-05-19 17:16:45 +0100

    added games

--- src/App.js
+++ src/App.js
@@ -1,21 +1,78 @@
-import logo from './logo.svg';
+import { logDOM } from "@testing-library/dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { games } from "./data";
 
-function Welcome(data){
-  return <a href="/{data.topic}">{data.topic}</a>
-}
+import logo from "./images/logo.png";
 
 function App() {
-  return ( 
-    <div className = "App">
-    <header>
-    Project Diva
-    </header>
-    <Welcome topic="Songs"/>
-    <Welcome topic="Modules"/>
-    <Welcome topic="Games"/>
-    </div>
+  return (
+    <Router>
+      <div className="header">
+        <h1>
+          Project Diva
+        </h1>
+        <img src={logo}></img>
+      </div>
+
+      <a href="/songs">Songs</a>
+      <a href="/modules">Modules</a>
+      <a href="/games">Games</a>
+
+      <Switch>
+        <Route path="/songs">
+          <Songs />
+        </Route>
+        <Route path="/modules">
+          <Modules />
+        </Route>
+        <Route path="/games">
+          <Games />
+        </Route>
+      </Switch>
+    </Router>
+
   );
+}
+
+function Songs() {
+  return (
+    <p>Here are songs</p>
+  )
+}
+
+function Modules() {
+  return (
+    <p>Here are modules</p>
+  )
+}
+
+function Games() {
+  return (
+    <>
+    <p>Here are games</p>
+    <div>
+    {
+      games.map((item)=>{
+        let output = "";
+        if (item.name.includes("Mirai")){
+          output =`Hatsune Miku: Project ${item.name} for the ${item.platform}, released ${item.released}`;
+        } else {
+          output =`Hatsune Miku: Project DIVA ${item.name} for the ${item.platform}, released ${item.released}`;
+        }
+        return (
+        <p>{output}</p>)}
+        )
+    }
+    </div>
+    </>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,87 @@
+export const games = [
+    {
+        "name": "",
+        "platform": "PSP",
+        "released": 2009
+    },
+    {
+        "name": "Arcade",
+        "platform": "Arcade",
+        "released": 2010
+    },
+    {
+        "name": "2nd",
+        "platform": "PSP",
+        "released": 2010
+    },
+    {
+        "name": "Extend",
+        "platform": "PSP",
+        "released": 2011
+    },
+    {
+        "name": "Mirai",
+        "platform": "3DS",
+        "released": 2012
+    },
+    {
+        "name": "F (Vita)",
+        "platform": "Vita",
+        "released": 2012
+    },
+    {
+        "name": "F (PS3)",
+        "platform": "PS3",
+        "released": 2013
+    },
+    {
+        "name": "Mirai 2",
+        "platform": "3DS",
+        "released": 2013
+    },
+    {
+        "name": "Arcade Future Tone",
+        "platform": "Arcade",
+        "released": 2013
+    },
+    {
+        "name": "F 2nd (Vita)",
+        "platform": "Vita",
+        "released": 2014
+    },
+    {
+        "name": "F 2nd (PS3)",
+        "platform": "PS3",
+        "released": 2014
+    },
+    {
+        "name": "Mirai DX",
+        "platform": "3DS",
+        "released": 2015
+    },
+    {
+        "name": "X",
+        "platform": "Vita",
+        "released": 2016
+    },
+    {
+        "name": "X HD",
+        "platform": "PS4",
+        "released": 2016
+    },
+    {
+        "name": "Future Tone",
+        "platform": "PS4",
+        "released": 2016
+    },
+    {
+        "name": "Future Tone DX",
+        "platform": "PS4",
+        "released": 2017
+    },
+    {
+        "name": "Mega Mix/Mega 39s",
+        "platform": "Switch",
+        "released": 2020
+    }
+]
